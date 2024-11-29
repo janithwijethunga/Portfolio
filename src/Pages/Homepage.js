afa65262ab74f1aa9d54dcc1eeef665fe1481f9a
Janith Wijethunga <janithwijethunga68@gmail.com>
2024-11-29 13:56:02 +0530

    About Me section

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,21 +1,34 @@
 import React from "react";
 import My1 from "../Assets/My1.png";
+import My2 from "../Assets/My2.png";
+import { FaDownload } from "react-icons/fa";
 
 function Homepage() {
   return (
     <div>
       <div className="bg-back w-full h-full">
-        <nav className="w-full flex justify-between items-center px-8 py-4 ">
+        <nav className=" fixed w-full flex justify-between items-center px-8 py-4 bg-gray-900">
           <h1 className="text-white font-semibold text-2xl">JW</h1>
-          <ul className="flex space-x-16 font-semibold text-xl font-poppins">
-            <li className="text-white">Home</li>
-            <li className="text-white">About</li>
-            <li className="text-white">Interests</li>
-            <li className="text-white">Education</li>
-            <li className="text-white">Projects</li>
-            <li className="text-white">Contact</li>
+          <ul className="flex space-x-16 font-semibold text-xl font-poppins ">
+            <li className="text-yell cursor-pointer">Home</li>
+            <li className="text-white cursor-pointer hover:text-yell">About</li>
+            <li className="text-white cursor-pointer hover:text-yell">
+              Interests
+            </li>
+            <li className="text-white cursor-pointer hover:text-yell">
+              Education
+            </li>
+            <li className="text-white cursor-pointer hover:text-yell">
+              Projects
+            </li>
+            <li className="text-white cursor-pointer hover:text-yell">
+              Contact
+            </li>
           </ul>
         </nav>
+        <br></br>
+        <br></br>
+        <br></br>
         <br></br>
         <br></br>
         {/*Home*/}
@@ -30,37 +43,17 @@ function Homepage() {
             <h1 className="text-white text-3xl font-bold">Hello!</h1>
             <h1 className="text-white text-7xl font-bold">
               I'M{" "}
-              <span className="text-yell text-8xl">JANITH <br></br>WIJETHUNGA</span>
+              <span className="text-yell text-8xl">
+                JANITH <br></br>WIJETHUNGA
+              </span>
             </h1>
-            <p className=" font-bold text-3xl text-white">FullStack Developer</p>
+            <p className=" font-bold text-3xl text-white">
+              FullStack Developer
+            </p>
             <p className="font-bold text-white text-3xl">UI/UX</p>
           </div>
         </div>
 
-        
-
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
         <br></br>
         <br></br>
         <br></br>
@@ -69,77 +62,104 @@ function Homepage() {
         <br></br>
         <br></br>
 
-        <h1>About</h1>
+        {/*About Me section*/}
+        <div className="justify-center flex">
+          {/*text*/}
+          <div className="font-poppins flex flex-col justify-center w-2/5">
+            {/*About Me heading*/}
+            <div className="">
+              <h1 className="text-white font-bold text-6xl">
+                About Me
+                <span className="text-white font-bold text-9xl opacity-15 absolute left-40">
+                  About Me
+                </span>
+              </h1>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
 
-        <div className="bg-gray-900 text-white w-full min-h-screen flex flex-col md:flex-row items-center justify-center px-8 md:px-16">
-      {/* Left Section: Text Content */}
-      <div className="md:w-1/2 text-center md:text-left">
-        {/* Title */}
-        <h1 className="text-5xl md:text-6xl font-bold relative">
-          About Me
-          <span className="absolute text-gray-700 opacity-20 top-2 left-0 w-full md:w-auto text-7xl md:text-9xl">
-            About
-          </span>
-        </h1>
-        <div className="mt-8 space-y-4">
-          <p className="text-lg">
-            I am an undergraduate student pursuing a{" "}
-            <span className="text-yellow-400 font-semibold">
-              BSc (Hons) in Information Technology
-            </span>{" "}
-            at the Sri Lanka Institute of Information Technology.
-          </p>
-          <p className="text-lg">
-            I am interested in{" "}
-            <span className="text-yellow-400 font-semibold">
-              Fullstack Development
-            </span>{" "}
-            and{" "}
-            <span className="text-yellow-400 font-semibold">UX/UI Design</span>{" "}
-            and enjoy learning new technologies to improve my skills. I work
-            well in team environments and always aim to deliver high-quality
-            work on time.
-          </p>
-          <p className="text-lg">
-            I see challenges as opportunities to grow and strive to contribute
-            positively to any organization I am part of.
-          </p>
+              <p className="text-white font-poppins font-bold text-lg">
+                {" "}
+                I am an undergraduate student pursuing a{" "}
+                <span className="text-yell">
+                  BSc (Hons) in Information Technology{" "}
+                </span>
+                at the sri lanka institute of information technology.
+              </p>
+              <br></br>
+              <p className="text-white font-poppins font-bold text-lg">
+                I am interested in{" "}
+                <span className="text-yell">Fullstack Development </span>
+                and <span className="text-yell">UX/UI Design </span>
+                and enjoy learning new technologies to improve my skills. I work
+                well in team environments and always aim to deliver high-quality
+                work on time.
+              </p>
+              <br></br>
+              <p className="text-white font-poppins font-bold text-lg">
+                I see challenges as opportunities to grow and strive to
+                contribute positively to any organization I am part of.
+              </p>
+              <br></br>
+              <div className="">
+                <a
+                  href="/janithwijethungaCV.pdf"
+                  download
+                  class="bg-yell hover:bg-yellow-500 text-black  hover:text-black font-bold py-2 px-4 rounded inline-flex items-center"
+                >
+                  <FaDownload />
+                  <span className="ml-2 text-lg">Download CV</span>
+                </a>
+              </div>
+
+              <div className="mt-8"></div>
+
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+            </div>
+          </div>
+
+          {/*My2*/}
+          <div className="size-4/12 ml-16">
+            <img src={My2} alt="Janith Wijethunga" />
+          </div>
         </div>
-        {/* Download Button */}
-        <div className="mt-8">
-          <button className="bg-yellow-400 text-black px-6 py-3 font-semibold rounded-lg shadow-lg flex items-center gap-2 hover:bg-yellow-500 transition duration-300">
-            Download CV
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 16v4h16v-4M4 16l8 8m0 0l8-8m-8 8V4"
-              />
-            </svg>
-          </button>
+
+        {/*My skills section*/}
+        <div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+
+          {/*skills*/}
+          <div className="flex justify-center items-center border-2 w-full">
+            <h1 className="text-white font-bold text-6xl transform -rotate-90 h-96 border-2 w-full">
+                My Skills
+                <span className="text-white font-bold text-9xl opacity-15 absolute left-5 w-full">
+                  My Skills
+                </span>
+              </h1>
+              
+            
+          
+
+          {/*icons*/}
+          <div>
+            iconswwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+            <br></br>
+          </div>
         </div>
+
+        <br></br>
       </div>
-
-      {/* Right Section: Profile Image */}
-      <div className="md:w-1/2 flex justify-center mt-12 md:mt-0">
-        <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full bg-yellow-400 flex items-center justify-center shadow-lg">
-          <img
-            src=""
-            alt="Profile"
-            className="w-56 h-56 md:w-72 md:h-72 rounded-full object-cover"
-          />
-        </div>
-      </div>
-    </div>
-
-
       </div>
     </div>
   );
