2ab9e920d8982c90f7b8d5c1b2c1986daa917b28
Janith Wijethunga <janithwijethunga68@gmail.com>
2025-07-02 19:55:39 +0530

    project setup & tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
-      colors: {
-        back:"#2D2D2D",
-        yell:"#FFBD39",
-        brw:"#202020",
-
-      },
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        Jomolhari: ['Jomolhari', 'serif'],
-        Michroma: ['Michroma', 'sans-serif'],
-      },
-      boxShadow: {
-        
-        sha: ['7px 8px 8.8px rgba(0, 0, 0, 100)']
-      },
+      
     },
   },
   plugins: [],
-}
+};
