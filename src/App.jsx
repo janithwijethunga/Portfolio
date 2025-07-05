47b4cc1acab14dfe6c89cfc5b9023a2e6d8cf005
Janith Wijethunga <janithwijethunga68@gmail.com>
2025-07-05 14:10:17 +0530

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Homepage from './Pages/Homepage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <div className='text-red-500 text-3xl'>
-        Janith
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+  ],
   theme: {
     extend: {
       
